8d751944747e6e62bb88601f4588c20c6b2c67c1
LorenzoCelli <lorenzo.celli00@gmail.com>
2023-06-15 12:58:16 +0200

    Watcher

--- shprocessor.py
+++ shprocessor.py
@@ -3,6 +3,10 @@ import os
 import glfw
 import pathlib
 import configparser
+from queue import Queue
+from datetime import datetime
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 from shader import *
 
@@ -15,42 +19,47 @@ VERT_EXTENSION_KEY = "Vertex extensions"
 FRAG_EXTENSION_KEY = "Fragment extensions"
 GEOM_EXTENSION_KEY = "Geometry extensions"
 
+main_thread_queue = Queue()
 
-def main():
 
-    if len(sys.argv) < 2:
-        print("Please provide the path to the configuration .ini file as the first parameter.")
-        return
+class Config:
+
+    def __init__(self, file):
+        config = configparser.ConfigParser()
+        config.read_file(file)
+        def_data = config['DEFAULT']
+
+        self.input_folder = os.path.normpath(def_data[INPUT_FOLDER_KEY])
+        self.template_folder = os.path.normpath(def_data[TEMPLATE_FOLDER_KEY])
+        self.output_folder = os.path.normpath(def_data[OUTPUT_FOLDER_KEY])
+        self.proc_folder = None
+        self.vert_extensions = tuple(json.loads(def_data[VERT_EXTENSION_KEY]))
+        self.frag_extensions = tuple(json.loads(def_data[FRAG_EXTENSION_KEY]))
+        self.geom_extensions = tuple(json.loads(def_data[GEOM_EXTENSION_KEY]))
+
+        if PROC_FOLDER_KEY in def_data:
+            self.proc_folder = os.path.normpath(def_data[PROC_FOLDER_KEY])
+
+
+def run(config_file, verbose=1):
 
     try:
-        f = open(sys.argv[1])
+        f = open(config_file)
     except IOError as e:
-        print(f"File '{sys.argv[1]}' not found ({e}).")
+        print(f"File '{config_file}' not found ({e}).")
         return
 
     # change the working directory so that every path is relative
     # to the configuration json file
-    os.chdir(pathlib.Path(sys.argv[1]).parent.resolve())
+    os.chdir(pathlib.Path(config_file).parent.resolve())
 
-    config = configparser.ConfigParser()
-    config.read_file(f)
-    def_data = config['DEFAULT']
+    cf = Config(f)
 
-    input_folder = os.path.normpath(def_data[INPUT_FOLDER_KEY])
-    template_folder = os.path.normpath(def_data[TEMPLATE_FOLDER_KEY])
-    output_folder = os.path.normpath(def_data[OUTPUT_FOLDER_KEY])
-    proc_folder = None
-    vert_extensions = tuple(json.loads(def_data[VERT_EXTENSION_KEY]))
-    frag_extensions = tuple(json.loads(def_data[FRAG_EXTENSION_KEY]))
-    geom_extensions = tuple(json.loads(def_data[GEOM_EXTENSION_KEY]))
-
-    if PROC_FOLDER_KEY in def_data:
-        proc_folder = os.path.normpath(def_data[PROC_FOLDER_KEY])
-
-    print(f"Input folder: {input_folder}")
-    print(f"Template folder: {template_folder}")
-    print(f"Output folder: {output_folder}")
-    print()
+    if verbose > 1:
+        print(f"Input folder: {cf.input_folder}")
+        print(f"Template folder: {cf.template_folder}")
+        print(f"Output folder: {cf.output_folder}")
+        print()
 
     if not glfw.init():
         print("Unable to initialize GLFW.")
@@ -66,38 +75,40 @@ def main():
 
     glfw.make_context_current(window)
 
-    collection = ShaderCollection(input_folder)
+    collection = ShaderCollection(cf.input_folder)
 
-    for file in os.listdir(input_folder):
+    for file in os.listdir(cf.input_folder):
 
         filename = os.fsdecode(file)
         sh_type = None
 
-        if filename.endswith(vert_extensions):
+        if filename.endswith(cf.vert_extensions):
             sh_type = ShaderType.VERTEX
 
-        elif filename.endswith(frag_extensions):
+        elif filename.endswith(cf.frag_extensions):
             sh_type = ShaderType.FRAGMENT
 
-        elif filename.endswith(geom_extensions):
+        elif filename.endswith(cf.geom_extensions):
             sh_type = ShaderType.GEOMETRY
 
         if not sh_type:
             continue
 
-        print(f"» {sh_type}: " + filename)
+        if verbose > 1:
+            print(f"» {sh_type}: " + filename)
+
         shader = collection.add(filename, sh_type)
 
-        if proc_folder:
+        if cf.proc_folder:
             # write the processed shaders code
-            out_file = open(os.path.join(proc_folder, filename), 'w+')
+            out_file = open(os.path.join(cf.proc_folder, filename), 'w+')
             out_file.write(shader.source)
             out_file.close()
 
-    template_env = Environment(loader=FileSystemLoader(template_folder),
+    template_env = Environment(loader=FileSystemLoader(cf.template_folder),
                                trim_blocks=True, lstrip_blocks=True)
 
-    for file in os.listdir(template_folder):
+    for file in os.listdir(cf.template_folder):
 
         filename = os.fsdecode(file)
         template = template_env.get_template(filename)
@@ -106,15 +117,91 @@ def main():
         output = template.render({"collection": collection})
 
         # write output file
-        out_file = open(os.path.join(output_folder, filename), 'w+')
+        out_file = open(os.path.join(cf.output_folder, filename), 'w+')
         out_file.write(output)
         out_file.close()
 
-    print()
-    print("Done!")
+    if verbose > 1:
+        print()
+
+    if verbose > 0:
+        print("Processing complete!")
 
     glfw.destroy_window(window)
     glfw.terminate()
+
+    return cf
+
+
+class FileChangeHandler(FileSystemEventHandler):
+
+    def __init__(self, cf_file, extensions):
+        self.extensions = extensions
+        self.cf_file = cf_file
+
+    def dispatch(self, event):
+
+        if event.is_directory:
+            return
+
+        _, ext = os.path.splitext(event.src_path)
+
+        if ext.endswith("~"):
+            # temporary file, we skip it
+            return
+
+        if len(self.extensions) == 0 or ext in self.extensions:
+            print(f"[{datetime.now().strftime('%H:%M:%S')}] Detected changes in {event.src_path}")
+            main_thread_queue.put(lambda: run(self.cf_file))
+
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("Please provide the path to the configuration .ini file as the first parameter.")
+        return
+
+    watch = sys.argv[1].lower() == "-w"
+
+    cf_path = sys.argv[1] if not watch else sys.argv[2]
+
+    cf = run(cf_path, 2)
+
+    if not watch:
+        return
+
+    print("Monitoring input and template folders for changes...")
+    print()
+
+    # event handler for shaders
+    eh_shaders = FileChangeHandler(
+        cf_path, cf.frag_extensions + cf.geom_extensions + cf.vert_extensions
+    )
+
+    # event handler for templates
+    eh_temp = FileChangeHandler(cf_path, ())
+
+    # input folder observer
+    in_obs = Observer()
+    in_obs.schedule(eh_shaders, cf.input_folder, recursive=True)
+    in_obs.start()
+
+    # template folder observer
+    temp_obs = Observer()
+    temp_obs.schedule(eh_temp, cf.template_folder, recursive=True)
+    temp_obs.start()
+
+    try:
+        while True:
+            f = main_thread_queue.get(True)
+            f()
+
+    except KeyboardInterrupt:
+        in_obs.stop()
+        temp_obs.stop()
+
+    in_obs.join()
+    temp_obs.join()
 
 
 if __name__ == "__main__":
